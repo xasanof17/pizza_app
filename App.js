d51f568b03e34cc1a4fbf3398fb4916e80df516b
Javohir <xasanof17@gmail.com>
2022-08-23 20:37:09 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import { HomeScreen } from "./src/screens";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  );
+}
